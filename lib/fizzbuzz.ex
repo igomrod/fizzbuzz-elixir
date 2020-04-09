1f136dda90f68a830116141325855c669c0b8017
Iván Gómez Rodríguez <igrodriguez@gradiant.org>
2020-04-09 13:59:20 +0200

    solution

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -12,7 +12,20 @@ defmodule Fizzbuzz do
       1
 
   """
-  def fizzbuzz(1) do
-    1
+  defp contains(container, contained) do
+    String.contains?(to_string(container), to_string(contained)) == true
+  end
+
+  defp is_fizz(num) do rem(num, 3) == 0 or contains(num, 3) end
+  defp is_buzz(num) do rem(num, 5) == 0 or contains(num, 5) end
+  defp is_fizzbuzz(num) do is_fizz(num) and is_buzz(num) end
+
+  def fizzbuzz(num) do
+    cond do
+      is_fizzbuzz(num) -> "fizzbuzz"
+      is_fizz(num) -> "fizz"
+      is_buzz(num) -> "buzz"
+      true -> num
+    end
   end
 end
--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -6,5 +6,36 @@ defmodule FizzbuzzTest do
     assert Fizzbuzz.fizzbuzz(1) == 1
   end
 
+  test "should return 2 when input 2" do
+    assert Fizzbuzz.fizzbuzz(2) == 2
+  end
+
+  test "should return fizz when input is divisible by 3" do
+    assert Fizzbuzz.fizzbuzz(3) == "fizz"
+    assert Fizzbuzz.fizzbuzz(6) == "fizz"
+    assert Fizzbuzz.fizzbuzz(9) == "fizz"
+  end
+
+  test "should return buzz when input is divisible by 5" do
+    assert Fizzbuzz.fizzbuzz(5) == "buzz"
+    assert Fizzbuzz.fizzbuzz(10) == "buzz"
+    assert Fizzbuzz.fizzbuzz(25) == "buzz"
+  end
+
+  test "should return fizzbuzz when input is divisible by 5 and 3" do
+    assert Fizzbuzz.fizzbuzz(15) == "fizzbuzz"
+  end
+
+  test "should return fizz when input contains 3" do
+    assert Fizzbuzz.fizzbuzz(3) == "fizz"
+    assert Fizzbuzz.fizzbuzz(13) == "fizz"
+    assert Fizzbuzz.fizzbuzz(23) == "fizz"
+  end
+
+  test "should return buzz when input contains 5" do
+    assert Fizzbuzz.fizzbuzz(3) == "fizz"
+    assert Fizzbuzz.fizzbuzz(13) == "fizz"
+    assert Fizzbuzz.fizzbuzz(23) == "fizz"
+  end
 
 end
